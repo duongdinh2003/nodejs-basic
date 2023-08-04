18813012eb11f5cba8d20a6c1234d6d4955b9c96
duongdinh2003 <lhdd1904@gmail.com>
2023-08-04 12:01:57 +0700

    'thirteenth'

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,4 +1,5 @@
 import pool from '../configs/connectDB';
+import multer from 'multer';
 
 let getHomepage = async (req, res) => {
     // logic
@@ -63,6 +64,40 @@ let postUpdateUser = async (req, res) => {
     return res.redirect('/');
 }
 
+let getUploadFilePage = async (req, res) => {
+    return res.render('uploadFile.ejs');
+}
+
+
+const upload = multer().single('profile_pic');
+
+let handleUploadFile = async (req, res) => {
+    // 'profile_pic' is the name of our file input field in the HTML form
+    // console.log(req.file);
+    upload(req, res, function (err) {
+        // req.file contains information of uploaded file
+        // req.body contains information of text fields, if there were any
+
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        else if (!req.file) {
+            return res.send('Please select an image to upload');
+        }
+        else if (err instanceof multer.MulterError) {
+            return res.send(err);
+        }
+        // version multer trên 1.4.3 sẽ gặp lỗi khi upload file
+        // else if (err) {
+        //     return res.send(err);
+        // }
+
+        // Display uploaded image for user validation
+        res.send(`You have uploaded this image: <hr/><img src="/image/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`);
+    });
+}
+
 module.exports = {
-    getHomepage, getDetailPage, createNewUser, deleteUser, getEditPage, postUpdateUser
+    getHomepage, getDetailPage, createNewUser, deleteUser, getEditPage, postUpdateUser,
+    getUploadFilePage, handleUploadFile
 }
