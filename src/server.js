9cf18ba2f7eeb16cb05922ce0a3c7ac4594627dd
duongdinh2003 <lhdd1904@gmail.com>
2023-08-06 10:43:42 +0700

    'fifteenth'

--- src/server.js
+++ src/server.js
@@ -7,10 +7,22 @@ import initAPIRoute from './router/api';
 
 require('dotenv').config();
 
+var morgan = require('morgan');
+
+
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 8080;
 console.log('>>> check port: ', port);
+
+app.use((req, res, next) => {
+    // check -> return res.send()
+    console.log('>>> run into my middleware');
+    console.log(req.method);
+    next();
+})
+
+app.use(morgan('combined'));
 
 // cấu hình cho post data
 app.use(express.urlencoded({ extended: true }));
@@ -24,6 +36,11 @@ initWebRoute(app);
 
 // init API route
 initAPIRoute(app);
+
+// handle 404 not found
+app.use((req, res) => {
+    return res.render('404.ejs');
+})
 
 
 app.listen(port, () => {
